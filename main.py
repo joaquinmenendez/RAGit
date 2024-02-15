c114bd1cdd7e7c3421f9fdc23febe477a2f65dd7
joaquinmen <joaquin14@gmail.com>
2024-02-15 13:09:55 -0300

    feature: perform more tests

--- main.py
+++ main.py
@@ -1,37 +1,57 @@
 import os
+import pandas as pd
 
-from video_to_subtitle import get_transcription_youtube, add_time_window_to_video
+from video_to_subtitle import get_transcription_youtube, \
+    concatenate_stt_captions
 from vector_db import transcription_to_db, load_db
-from llm import answer_question_vertex
+from llm import answer_question_vertex, add_punctuation
 from embeddings import calculate_tokens
-from check_grounding import check_grounding, fact_formatter, add_citations
+from check_grounding import check_grounding, fact_formatter, add_citations, \
+    links_to_references
 
+# Get transcripts directly from YouTube ---------------------------------------
 url = "https://www.youtube.com/watch?v=NgbEL2HbXWw"
 
 raw_transcript, chunked_transcript = get_transcription_youtube(url)
 print(f'Number of tokens: {calculate_tokens(raw_transcript)}')
 
+# Add punctuation (YouTube's generated transcripts does not have punctuation) --
+corrected_transcription = add_punctuation(raw_transcript)
+
+# Using StT transcriptions -----------------------------------------------------
+stt_df = pd.read_csv(
+    './multimedia/el_origen_de_los_distintos_acentos_de_argentina_datazo.csv')
+chunked_transcript_stt = concatenate_stt_captions(stt_df.to_dict('records'),
+                                                  num_words=130)
+[c.update({'url': url}) for c in chunked_transcript_stt]
+
+# Mount Chroma DB --------------------------------------------------------------
 if os.path.exists("./chroma_db"):
     db = load_db()
 else:
-    db = transcription_to_db(transcriptions=chunked_transcript)
+    # db = transcription_to_db(transcriptions=chunked_transcript)
+    db = transcription_to_db(transcriptions=chunked_transcript_stt)
 
+# Define question --------------------------------------------------------------
+query = 'Cómo se llama el libro de los acentos?'
 
-query = 'Que son los miembros manija?'
+# LLM naive approach (use entire transcript) -----------------------------------
+if calculate_tokens(raw_transcript) < 30000:
+    answer = answer_question_vertex(question=query, context=corrected_transcription)
+    print(f'Respuesta: {answer}')
+else:
+    raise NotImplementedError('Transcription is too long')
 
-# LLM naive approach
-answer = answer_question_vertex(question=query, context=raw_transcript)
-print(f'Respuesta: {answer}')
+# Chroma approach with check grounding ----------------------------------------
+response = db.similarity_search(query, k=3)  # TODO: Langchain retrievers
 
-# Chroma approach
-response = db.similarity_search(query, k=3)
 answer_context = '\n\n'.join([i.page_content for i in response])
-answer_url = add_time_window_to_video(url, metadata=response[0].metadata)
 answer = answer_question_vertex(question=query, context=answer_context)
-print(f'Respuesta: {answer}\nLink:{answer_url}')
 
-# Test check grounding
 facts = fact_formatter([i.page_content for i in response])
+
 grounded = check_grounding(answer_candidate=answer, facts=facts)
-answer_with_citations = add_citations(answer_candidate=answer, grounded_response=grounded)
+answer_with_citations, references = add_citations(answer_candidate=answer,
+                                                  grounded_response=grounded)
 print(answer_with_citations)
+print(links_to_references(response, references))
